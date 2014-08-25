5e2c0ba0b89971e9780cd4c27b41e90d9c982b00
Ashish Sharma <ashish.sharma@oodlestechnologies.com>
2014-08-25 16:31:16 +0530

    rating directive updated

--- directives/Rating.js
+++ directives/Rating.js
@@ -1,13 +1,19 @@
-angular.module('rating', []).directive('starating', function($compile) {
+var max = 5;
+angular.module('rating', []).directive('starRating', function($compile) {
   return {
     template: '<div></div>',
     replace: true,
-    link: function(scope, element) {
-      scope.rating = 0;
+    require:"ngModel",
+    link: function(scope, element, attr, ngModel) {
+      scope.$watch(function(){
+        return ngModel.$modelValue;
+    }, function(modelValue){
+      scope.rating = modelValue;
+      updateStars(modelValue, stars);
+    });
       var el = angular.element('<span/>');
-      var max = 5;
       var value = 0;
-      var stars = []
+      var stars = [];
       for(var i =1;i<=max;i++){
         var star = angular.element('<span data-id="'+i+'" class="glyphicon glyphicon-star-empty" ng-click="rating='+i+';getRate('+i+')"></span>');
         stars.push(star);
@@ -16,27 +22,53 @@ angular.module('rating', []).directive('starating', function($compile) {
           this.classList.remove('glyphicon-star-empty');
           this.classList.add('glyphicon-star');
           value = angular.element(this).attr('data-id');
-          updateStars(value);
+          updateStars(value, stars);
         }).bind('mouseout', function(){
-          updateStars(value);
+          updateStars(value, stars);
         });
       }
-      function updateStars(val){
-        for(var j=0;j<max;j++){
-          if(stars[j].attr('data-id') <= val){
-            stars[j].removeClass("glyphicon-star-empty");
-            stars[j].addClass("glyphicon-star");
-          }else{
-            stars[j].removeClass("glyphicon-star");
-            stars[j].addClass("glyphicon-star-empty");
-          }
-        }
-      }
       el.bind('mouseout', function(){
-        updateStars(scope.rating);
+        updateStars(scope.rating, stars);
       });
       $compile(el)(scope);
       element.append(el);
     }
   }
+}).directive('showRating', function($compile) {
+  return {
+    template: '<div></div>',
+    replace: true,
+    require:"ngModel",
+    link: function(scope, element, attr, ngModel) {
+      scope.$watch(function(){
+        return ngModel.$modelValue;
+    }, function(modelValue){
+        console.log(modelValue);
+        var el = angular.element('<span/>');
+        var value = 0;
+        var stars = [];
+        for(var i =1;i<=max;i++){
+          var star = angular.element('<span data-id="'+i+'" class="glyphicon glyphicon-star-empty"></span>');
+          stars.push(star);
+          el.append(star);
+        }
+        updateStars(modelValue, stars);
+        $compile(el)(scope);
+        element.append(el);
+        
+    });
+    }
+  }
 });
+
+function updateStars(val, stars){
+  for(var j=0;j<max;j++){
+    if(stars[j].attr('data-id') <= val){
+      stars[j].removeClass("glyphicon-star-empty");
+      stars[j].addClass("glyphicon-star");
+    }else{
+      stars[j].removeClass("glyphicon-star");
+      stars[j].addClass("glyphicon-star-empty");
+    }
+  }
+}
